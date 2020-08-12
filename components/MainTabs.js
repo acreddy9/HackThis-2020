58aeaacb3ecefff18576655795610a08e2e65039
acreddy9 <55812515+acreddy9@users.noreply.github.com>
2020-08-12 01:14:21 -0500

    Changed tab bar style

--- components/MainTabs.js
+++ components/MainTabs.js
@@ -15,44 +15,46 @@ function MainTabs(props) {
     const user = props.extraData
     
     return (
-       // <NavigationContainer>
-            <Tab.Navigator
-                screenOptions={({ route }) => ({
-                    tabBarIcon: ({ focused, color, size }) => {
-                        let iconName;
-                        if (route.name === "Profile") {
-                            iconName = focused ? "md-person" : "md-person";
-                        } else if (route.name === "Preferences") {
-                            iconName = focused ? "md-settings" : "md-settings";
-                        } else if (route.name === "Courses") {
-                            iconName = focused ? "md-heart" : "md-heart";
-                        } else if (route.name === "Chat") {
-                            iconName = focused ? "ios-chatbubbles" : "ios-chatbubbles";
-                        }
+        <Tab.Navigator
+            screenOptions={({ route }) => ({
+                tabBarIcon: ({ focused, color, size }) => {
+                    let iconName;
+                    if (route.name === "Profile") {
+                        iconName = focused ? "md-person" : "md-person";
+                    } else if (route.name === "Preferences") {
+                        iconName = focused ? "md-settings" : "md-settings";
+                    } else if (route.name === "Courses") {
+                        iconName = focused ? "md-heart" : "md-heart";
+                    } else if (route.name === "Chat") {
+                        iconName = focused ? "ios-chatbubbles" : "ios-chatbubbles";
+                    }
 
-                        return <Ionicons name={iconName} size={size} color={color} />;
-                     },
-                     
-                     tabBarLabel: () => {return null}
-                     
-                })}
-
-                // Icon change when clicking the tab
-                tabBarOptions={{
-                    activeTintColor: "#E9C46A",
-                    inactiveTintColor: "#264653",
-                    adaptive: true,
-                    tabStyle: {
-                        padding: 0, margin: 10,   
+                    return <Ionicons name={iconName} size={size} color={color} />;
                     },
-                }}
-          >
-                <Tab.Screen name="Profile" component={ProfileScreen} initialParams={{ data: user }}/>
-                <Tab.Screen name="Preferences" component={PreferencesScreen} initialParams={{ data: user }}/>
-                <Tab.Screen name="Courses" component={CoursesScreen} initialParams={{ data: user }}/>
-                <Tab.Screen name="Chat" component={ChatScreen} initialParams={{ data: user }}/>
-            </Tab.Navigator>
-        // </NavigationContainer> 
+                    
+                    tabBarLabel: () => {return null}
+                    
+            })}
+
+            // Icon change when clicking the tab
+            tabBarOptions={{
+                activeTintColor: "#ffffff",
+                inactiveTintColor: "#ffffff",
+                activeBackgroundColor: "#AFABF2",
+                adaptive: true,
+                tabStyle: {
+                    padding: 0, margin: 10,
+                },
+                style: {
+                    backgroundColor: "#6c63ff",
+                }
+            }}
+        >
+            <Tab.Screen name="Profile" component={ProfileScreen} initialParams={{ data: user }}/>
+            <Tab.Screen name="Preferences" component={PreferencesScreen} initialParams={{ data: user }}/>
+            <Tab.Screen name="Courses" component={CoursesScreen} initialParams={{ data: user }}/>
+            <Tab.Screen name="Chat" component={ChatScreen} initialParams={{ data: user }}/>
+        </Tab.Navigator>
     );
 }
 
